bbf54f4cd0ff2e20bd097582c14e7578d673d884
Yoriyasu Yano <yorinasub17@gmail.com>
2014-12-14 22:15:16 -0800

    Support for gpuarrays

--- sciguppy/correlate.py
+++ sciguppy/correlate.py
@@ -1,17 +1,21 @@
-__all__ = ['FULL', 'VALID', 'correlate']
+__all__ = ['FULL', 'VALID', 'CPU', 'GPU', 'correlate']
 
 import os
 import numpy
 import math
 import pycuda.driver as cuda
 import pycuda.autoinit
+import pycuda.gpuarray
 from pycuda.compiler import SourceModule
+from .utils import as_gpu, as_cpu
 
 CUR_DIR = os.path.dirname(os.path.realpath(__file__))
 CACHE_DIR = os.path.join(CUR_DIR, 'cache')
 
 FULL = 'full'
 VALID = 'valid'
+CPU = 'cpu'
+GPU = 'gpu'
 MAX_BLOCK_SIZE = 512
 
 mod = SourceModule(open(os.path.join(CUR_DIR, 'kernel/correlate.cu')).read(), cache_dir=CACHE_DIR)
@@ -19,34 +23,38 @@ correlate_kernel = mod.get_function('correlate_kernel')
 d_ax_size = mod.get_global('d_ax_size')[0]
 d_ay_size = mod.get_global('d_ay_size')[0]
 d_aout_size = mod.get_global('d_aout_size')[0]
+d_padding = mod.get_global('d_padding')[0]
 
-def correlate(a1, a2, mode=FULL):
+def correlate(a1, a2, mode=FULL, return_type=CPU):
+    x1, y1, z1 = a1.shape
     x2, y2, z2 = a2.shape
     if mode == FULL:
+        # In FULL mode, cycle through minimum overlap, including those where
+        # the array is out of bounds. Out of bound values are treated as 0s
+        outx, outy, outz = x1+x2-1, y1+y2-1, z1+z2-1
         xpad = x2 - 1
         ypad = y2 - 1
         zpad = z2 - 1
-        a1 = numpy.pad(a1, ((xpad, xpad), (ypad, ypad), (zpad, zpad)), 'constant', constant_values=0)
+    else:
+        outx, outy, outz = x1-x2+1, y1-y2+1, z1-z2+1
+        xpad = 0
+        ypad = 0
+        zpad = 0
 
-    x1, y1, z1 = a1.shape
-    outx, outy, outz = x1-x2+1, y1-y2+1, z1-z2+1
-    aout = numpy.zeros((outx, outy, outz))
-
-    a1 = a1.astype(numpy.float32)
-    d_a1 = cuda.mem_alloc(a1.nbytes)
-    cuda.memcpy_htod(d_a1, a1)
+    d_a1 = as_gpu(a1)
     cuda.memcpy_htod(d_ax_size, numpy.array(a1.shape, dtype=numpy.int32))
-    a2 = a2.astype(numpy.float32)
-    d_a2 = cuda.mem_alloc(a2.nbytes)
-    cuda.memcpy_htod(d_a2, a2)
+    d_a2 = as_gpu(a2)
     cuda.memcpy_htod(d_ay_size, numpy.array(a2.shape, dtype=numpy.int32))
-    aout = aout.astype(numpy.float32)
-    d_aout = cuda.mem_alloc(aout.nbytes)
-    cuda.memcpy_htod(d_aout_size, numpy.array(aout.shape, dtype=numpy.int32))
+    d_aout = pycuda.gpuarray.zeros((outx, outy, outz), numpy.float32)
+    cuda.memcpy_htod(d_aout_size, numpy.array(d_aout.shape, dtype=numpy.int32))
+    cuda.memcpy_htod(d_padding, numpy.array((xpad, ypad, zpad), dtype=numpy.int32))
 
     thread_size = min(outx*outy*outz, MAX_BLOCK_SIZE)
     block_size = max(int(math.ceil(outx*outy*outz / float(thread_size))), 1)
     correlate_kernel(d_a1, d_a2, d_aout, 
             block=(thread_size,1,1), grid=(block_size,1,1))
-    cuda.memcpy_dtoh(aout, d_aout)
-    return aout
+
+    if return_type == CPU:
+        return as_cpu(d_aout)
+    else:
+        return as_gpu(d_aout)
--- sciguppy/kernel/correlate.cu
+++ sciguppy/kernel/correlate.cu
@@ -2,10 +2,13 @@
 __device__ __constant__ int d_ax_size[3];
 __device__ __constant__ int d_ay_size[3];
 __device__ __constant__ int d_aout_size[3];
+__device__ __constant__ int d_padding[3];
 
+// Signal correlation kernel. Only works with 3D arrays. One thread per output entry.
 __global__ void correlate_kernel(float *ax, float *ay, float *aout) {
 
-    //const int ax_w = d_ax_size[0]; // Unused
+    // Extract dimensions and padding
+    const int ax_w = d_ax_size[0];
     const int ax_h = d_ax_size[1];
     const int ax_d = d_ax_size[2];
     const int ay_w = d_ay_size[0];
@@ -14,22 +17,44 @@ __global__ void correlate_kernel(float *ax, float *ay, float *aout) {
     const int aout_w = d_aout_size[0];
     const int aout_h = d_aout_size[1];
     const int aout_d = d_aout_size[2];
+    const int xpad = d_padding[0];
+    const int ypad = d_padding[1];
+    const int zpad = d_padding[2];
  
+    // Get the id, and make sure it is not out of bounds
     const int id = threadIdx.x + blockIdx.x * blockDim.x;
     if (id >= aout_w * aout_h * aout_d) {
         return;
     }
 
+    // Now get the coordinates in the output matrix
     const int x = id / (aout_h*aout_d);
     const int y = (id % (aout_h*aout_d)) / aout_d;
     const int z = (id % (aout_h*aout_d)) % aout_d;
+
     int ax_id, ay_id, i, j, k;
+    int ax_x, ax_y, ax_z;
     float sum = 0.0;
 
+    // Each output entry is a reduction of matrix dot product between the two
+    // inputs at that point
     for (i = 0; i < ay_w; ++i) {
         for (j = 0; j < ay_h; ++j) {
             for (k = 0; k < ay_d; ++k) {
-                ax_id = ((x+i) * ax_h * ax_d) + ((y+j) * ax_d) + (z+k);
+                // Bounds check. If out of bound, ignore because ax value is 0.
+                ax_x = (x+i)-xpad;
+                ax_y = (y+j)-ypad;
+                ax_z = (z+k)-zpad;
+                if ((ax_x < 0) ||
+                    (ax_y < 0) ||
+                    (ax_z < 0) ||
+                    (ax_x >= ax_w) ||
+                    (ax_y >= ax_h) ||
+                    (ax_z >= ax_d))
+                    continue;
+
+                // Correlation is sum of dot product between overlapping points
+                ax_id = (ax_x * ax_h * ax_d) + (ax_y * ax_d) + ax_z;
                 ay_id = (i * ay_h * ay_d) + (j * ay_d) + k;
                 sum += (ax[ax_id] * ay[ay_id]);
             }
