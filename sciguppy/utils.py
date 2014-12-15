8adec284370d72a0a93012dcd921f510a46a1f41
Yoriyasu Yano <yorinasub17@gmail.com>
2014-12-14 21:43:34 -0800

    Utility function to guarantee GPUArrays or CPUArrays

---
+++ sciguppy/utils.py
@@ -0,0 +1,28 @@
+__all__ = ['as_gpu', 'as_cpu']
+
+import numpy
+import pycuda.gpuarray as gpuarray
+import pycuda.driver as cuda
+import pycuda.autoinit
+
+def as_gpu(a):
+    """ Returns a as a GPUArray
+
+    If a is already a GPUArray, simply returns it. Otherwise, copies the array
+    to the GPU and returns the reference.
+    """
+    if type(a) == gpuarray.GPUArray:
+        return a
+    else:
+        return gpuarray.to_gpu(a)
+
+def as_cpu(a):
+    """ Returns a as a numpy array
+
+    If a is already a numpy array, simply returns it. Otherwise, copies the
+    array to the CPU and returns the reference.
+    """
+    if type(a) == gpuarray.GPUArray:
+        return a.get()
+    else:
+        return a
